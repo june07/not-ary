39cff48356e84772bd648a7111435c3ad422fc66
Adrian <adrian@awardell.com>
2023-06-16 19:54:23 -0700

    .

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,6 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/not-ary/',
     plugins: [
         vue({
             template: { transformAssetUrls },
