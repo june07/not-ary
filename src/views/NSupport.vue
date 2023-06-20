b63a9b787311ceeecf36e5e0f9a0d04e77a2034c
Will <11353590+june07@users.noreply.github.com>
2023-06-19 21:11:26 -0700

    Update NSupport.vue

--- src/views/NSupport.vue
+++ src/views/NSupport.vue
@@ -52,19 +52,19 @@
 
                 <li><b>Content Creation:</b> Developing high-quality practice exams requires time and effort. With your contribution, we can allocate resources to create in-depth study resources that will benefit notary exam candidates in their preparation journey.</li>
 
-                <li><b>Technical Upgrades and Maintenance:</b> Technology is constantly evolving, and it is crucial for us to stay up-to-date to provide a seamless experience for our users. Your support will help us invest in technical upgrades, server maintenance, and security enhancements to ensure that notary-exam.com remains accessible and reliable for all users.</li>
+                <li><b>Technical Upgrades and Maintenance:</b> Technology is constantly evolving, and it is crucial for us to stay up-to-date to provide a seamless experience for our users. Your support will help us invest in technical upgrades, server maintenance, and security enhancements to ensure that Not-Ary.com remains accessible and reliable for all users.</li>
 
-                <li><b>Server and Hosting Costs:</b> Running a website involves ongoing expenses for server hosting, domain registration, and bandwidth. Your donation will help cover these operational costs, ensuring that notary-exam.com remains available to users around the world.</li>
+                <li><b>Server and Hosting Costs:</b> Running a website involves ongoing expenses for server hosting, domain registration, and bandwidth. Your donation will help cover these operational costs, ensuring that Not-Ary.com remains available to users around the world.</li>
             </ol>
             <p class="my-8">
-                Your donation, no matter the size, will make a significant impact in supporting the growth and development of Not-ary.com. I am grateful for your support in our mission to provide valuable resources to aspiring notaries.</p>
+                Your donation, no matter the size, will make a significant impact in supporting the growth and development of Not-Ary.com. I am grateful for your support in our mission to provide valuable resources to aspiring notaries.</p>
             <p class="my-8">
-                Thank you for being a part of the Not-ary.com community!</p>
+                Thank you for being a part of the Not-Ary.com community!</p>
             <p class="my-8">Best regards,<br>
                 <icon-base iconName="signature" height="100" width="150" viewBox="150 0 100 200"><icon-signature></icon-signature></icon-base>
             </p>
             <p class="my-2">Adrian Wardell</p>
-            <p class="my-2">Founder, Not-ary.com</p>
+            <p class="my-2">Founder, Not-Ary.com</p>
             </p>
         </div>
     </v-container>
